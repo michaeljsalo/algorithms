7cafcd087f0ebd1c60af2e094b496489636c5b88
Michael J Salo <michael@salo.nyc>
2020-09-28 19:09:59 -0500

    Clarify digit parsing

--- digits.js
+++ digits.js
@@ -29,19 +29,32 @@ function binaryToDecimal (binaryString = '0') {
     return result
 }
 
-// Get a character to represent any integer from 0 to 36
+// Get a character to represent any integer from 0 to 35
+// 10: A
+// 35: Z
 function getDigitLetter (digit = 0) {
     digit = Math.floor(Number(digit))
     let result
+
+    // Digit is in decimal range
     if (0 <= digit && digit <= 9) {
         result = String(digit)
-    } else if (10 <= digit && digit <= 36) {
+
+    // Digit is in letter range
+    } else if (10 <= digit && digit <= 35) {
         result = String.fromCharCode(digit + 55)
+
+    // Digit is unknown
+    } else {
+        result = '?'
     }
+
     return result
 }
 
 // Get a number value for any character from 0 to Z
+// A: 10
+// Z: 35
 function getDigitValue (digitChar = '0') {
     const digitCharCode = String(digitChar).trim().toUpperCase().charCodeAt(0)
     let result
@@ -131,6 +144,8 @@ console.log(argNum + " rounds to " + roundNumber(argNum, -2))
 console.log(argNum + " to binary: " + decimalToBinary(argNum))
 console.log(argNum + " to decimal: " + binaryToDecimal(argNum))
 
+console.log(argNum + " to binary (via any): " + decimalToAny(argNum, 2))
+console.log(argNum + " to decimal (via any): " + anyToDecimal(argNum, 2))
 console.log(argNum + " to octal (via any): " + decimalToAny(argNum, 8))
 console.log(argNum + " to decimal (via any): " + anyToDecimal(argNum, 8))
 console.log(argNum + " to hex (via any): " + decimalToAny(argNum, 16))
