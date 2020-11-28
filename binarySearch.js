74a01e445a5c21f962df8f1e540dc56c14af59e7
Michael J Salo <michael@salo.nyc>
2020-11-27 18:28:14 -0600

    Update binary search implementations

--- binarySearch.js
+++ binarySearch.js
@@ -1,104 +1,86 @@
-// Input : arr[] = {1, 3, 5, 7, 8, 9}
-//         x = 5
-// Output : Element found!
+function binarySearch (arr = [], needle) {
+  let lo = 0
+  let hi = arr.length - 1
+  let mid
 
-// Input : arr[] = {1, 3, 5, 7, 8, 9}
-//         x = 6
-// Output : Element not found!
+  while (lo <= hi) {
+    mid = lo + Math.floor((hi - lo) / 2)
 
+    // Found it
+    if (arr[mid] === needle) {
+      return true
 
-function binarySearch(arr = [], needle = -1, lowIndex = 0, highIndex = arr.length-1) {
-	// console.log(lowIndex + ' - ' + highIndex)
+      // Too high
+    } else if (arr[mid] > needle) {
+      hi = mid - 1
 
-	let midIndex = Math.floor((lowIndex + highIndex) / 2);
+      // Too low
+    } else if (arr[mid] < needle) {
+      lo = mid + 1
+    }
+  }
 
-	// Base case: found it
-	if (arr[midIndex] === needle) {
-		return true;
-	}
-
-	// Base case: not found
-	if (lowIndex >= highIndex) {
-		return false;
-	}
-
-	// Recursive case: go lower
-	if (arr[midIndex] > needle) {
-		return binarySearch(arr, needle, lowIndex, midIndex - 1);
-	}
-
-	// Recursive case: go higher
-	if (arr[midIndex] < needle) {
-		return binarySearch(arr, needle, midIndex + 1, highIndex);
-	}
+  // Didn't find it
+  return false
 }
 
-function binarySearchClosure(arr = [], needle = -1) {
-	let lowIndex = 0;
-	let highIndex = arr.length - 1;
+function binarySearchRecursive (arr = [], needle, lo = 0, hi = arr.length - 1) {
+  const mid = lo + Math.floor((hi - lo) / 2)
 
-	return (function binarySearchInner(lowIndex, highIndex) {
-		// console.log(lowIndex + ' - ' + highIndex)
+  // Base case: found it
+  if (arr[mid] === needle) {
+    return true
+  }
 
-		let midIndex = Math.floor((lowIndex + highIndex) / 2);
+  // Base case: didn't find it
+  if (lo >= hi) {
+    return false
+  }
 
-		// Base case: found it
-		if (arr[midIndex] === needle) {
-			return true;
-		}
+  // Recursive case: search lower
+  if (arr[mid] > needle) {
+    return binarySearchRecursive(arr, needle, lo, mid - 1)
+  }
 
-		// Base case: not found
-		if (lowIndex >= highIndex) {
-			return false;
-		}
-
-		// Recursive case: go lower
-		if (arr[midIndex] > needle) {
-			return binarySearchInner(lowIndex, midIndex - 1);
-		}
-
-		// Recursive case: go higher
-		if (arr[midIndex] < needle) {
-			return binarySearchInner(midIndex + 1, highIndex);
-		}
-	}(lowIndex, highIndex));
+  // Recursive case: search higher
+  if (arr[mid] < needle) {
+    return binarySearchRecursive(arr, needle, mid + 1, hi)
+  }
 }
 
-function binarySearchLoop(arr = [], needle = -1, lowIndex = 0, highIndex = arr.length-1) {
-	// console.log(lowIndex + ' - ' + highIndex)
+function binarySearchClosure (arr = [], needle) {
+  const lo = 0
+  const hi = arr.length - 1
 
-	let midIndex;
-	while (true) {
-		midIndex = Math.floor((lowIndex + highIndex) / 2);
+  return (function binarySearchInner (lo, hi) {
+    const mid = lo + Math.floor((hi - lo) / 2)
 
-		// Base case: found it
-		if (arr[midIndex] === needle) {
-			return true;
-		}
+    // Base case: found it
+    if (arr[mid] === needle) {
+      return true
+    }
 
-		// Base case: not found
-		if (lowIndex >= highIndex) {
-			return false;
-		}
+    // Base case: didn't find it
+    if (lo >= hi) {
+      return false
+    }
 
-		// Recursive case: go lower
-		if (arr[midIndex] > needle) {
-			highIndex = midIndex - 1;
-			continue;
-		}
+    // Recursive case: search lower
+    if (arr[mid] > needle) {
+      return binarySearchInner(lo, mid - 1)
+    }
 
-		// Recursive case: go higher
-		if (arr[midIndex] < needle) {
-			lowIndex = midIndex + 1;
-			continue;
-		}
-	}
+    // Recursive case: search higher
+    if (arr[mid] < needle) {
+      return binarySearchInner(mid + 1, hi)
+    }
+  }(lo, hi))
 }
 
-
-const arr = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
-
+const arr = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]
 console.log(arr)
+
+console.log('\nbinarySearch')
 console.log('-10: ' + binarySearch(arr, -10))
 console.log('0: ' + binarySearch(arr, 0))
 console.log('1: ' + binarySearch(arr, 1))
@@ -111,10 +93,12 @@ console.log('36: ' + binarySearch(arr, 36))
 console.log('37: ' + binarySearch(arr, 37))
 console.log('100: ' + binarySearch(arr, 100))
 
-// console.log('5: ' + binarySearchClosure(arr, 5))
-// console.log('6: ' + binarySearchClosure(arr, 6))
-// console.log('7: ' + binarySearchClosure(arr, 7))
+console.log('\nbinarySearchRecursive')
+console.log('5: ' + binarySearchRecursive(arr, 5))
+console.log('6: ' + binarySearchRecursive(arr, 6))
+console.log('7: ' + binarySearchRecursive(arr, 7))
 
-// console.log('5: ' + binarySearchLoop(arr, 5))
-// console.log('6: ' + binarySearchLoop(arr, 6))
-// console.log('7: ' + binarySearchLoop(arr, 7))
+console.log('\nbinarySearchClosure')
+console.log('5: ' + binarySearchClosure(arr, 5))
+console.log('6: ' + binarySearchClosure(arr, 6))
+console.log('7: ' + binarySearchClosure(arr, 7))
