4cdb9263e3956d1802661273192980a508cc10a2
Michael J Salo <michael@salo.nyc>
2020-11-28 17:55:08 -0600

    Add to binary search implementations

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,4 @@
-function binarySearch (arr = [], needle) {
+function binarySearch (arr = [], target) {
   let lo = 0
   let hi = arr.length - 1
   let mid
@@ -7,48 +7,48 @@ function binarySearch (arr = [], needle) {
     mid = lo + Math.floor((hi - lo) / 2)
 
     // Found it
-    if (arr[mid] === needle) {
-      return true
+    if (arr[mid] === target) {
+      return mid
 
       // Too high
-    } else if (arr[mid] > needle) {
+    } else if (arr[mid] > target) {
       hi = mid - 1
 
       // Too low
-    } else if (arr[mid] < needle) {
+    } else if (arr[mid] < target) {
       lo = mid + 1
     }
   }
 
   // Didn't find it
-  return false
+  return -1
 }
 
-function binarySearchRecursive (arr = [], needle, lo = 0, hi = arr.length - 1) {
+function binarySearchRecursive (arr = [], target, lo = 0, hi = arr.length - 1) {
   const mid = lo + Math.floor((hi - lo) / 2)
 
   // Base case: found it
-  if (arr[mid] === needle) {
-    return true
+  if (arr[mid] === target) {
+    return mid
   }
 
   // Base case: didn't find it
   if (lo >= hi) {
-    return false
+    return -1
   }
 
   // Recursive case: search lower
-  if (arr[mid] > needle) {
-    return binarySearchRecursive(arr, needle, lo, mid - 1)
+  if (arr[mid] > target) {
+    return binarySearchRecursive(arr, target, lo, mid - 1)
   }
 
   // Recursive case: search higher
-  if (arr[mid] < needle) {
-    return binarySearchRecursive(arr, needle, mid + 1, hi)
+  if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, hi)
   }
 }
 
-function binarySearchClosure (arr = [], needle) {
+function binarySearchClosure (arr = [], target) {
   const lo = 0
   const hi = arr.length - 1
 
@@ -56,49 +56,148 @@ function binarySearchClosure (arr = [], needle) {
     const mid = lo + Math.floor((hi - lo) / 2)
 
     // Base case: found it
-    if (arr[mid] === needle) {
-      return true
+    if (arr[mid] === target) {
+      return mid
     }
 
     // Base case: didn't find it
     if (lo >= hi) {
-      return false
+      return -1
     }
 
     // Recursive case: search lower
-    if (arr[mid] > needle) {
+    if (arr[mid] > target) {
       return binarySearchInner(lo, mid - 1)
     }
 
     // Recursive case: search higher
-    if (arr[mid] < needle) {
+    if (arr[mid] < target) {
       return binarySearchInner(mid + 1, hi)
     }
   }(lo, hi))
 }
 
-const arr = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]
+function binarySearchFirst (arr = [], target) {
+  let lo = 0
+  let hi = arr.length - 1
+  let mid
+  let result = -1
+
+  while (lo <= hi) {
+    mid = lo + Math.floor((hi - lo) / 2)
+
+    // Found a match
+    if (arr[mid] === target) {
+      result = mid
+    }
+
+    // Too high or same
+    if (arr[mid] >= target) {
+      hi = mid - 1
+
+      // Too low
+    } else if (arr[mid] < target) {
+      lo = mid + 1
+    }
+  }
+
+  // Return best match
+  return result
+}
+
+function binarySearchLast (arr = [], target) {
+  let lo = 0
+  let hi = arr.length - 1
+  let mid
+  let result = -1
+
+  while (lo <= hi) {
+    mid = lo + Math.floor((hi - lo) / 2)
+
+    // Found a match
+    if (arr[mid] === target) {
+      result = mid
+    }
+
+    // Too high
+    if (arr[mid] > target) {
+      hi = mid - 1
+
+    // Too low or same
+    } else if (arr[mid] <= target) {
+      lo = mid + 1
+    }
+  }
+
+  // Return best match
+  return result
+}
+
+// Find where to insert a given target
+// If the value already exists, insert after it
+function binarySearchInsert (arr = [], target) {
+  let lo = 0
+  let hi = arr.length - 1
+  let mid
+
+  while (lo <= hi) {
+    mid = lo + Math.floor((hi - lo) / 2)
+
+    // Too high
+    if (arr[mid] > target) {
+      hi = mid - 1
+
+    // Too low or same
+    } else if (arr[mid] <= target) {
+      lo = mid + 1
+    }
+  }
+
+  // Go with the higher index that we landed on
+  return Math.max(lo, hi)
+}
+
+const arr = [1, 3, 3, 5, 7, 11, 11, 11, 13, 17, 19, 23, 29, 31, 33, 33, 33, 37, 41, 43, 47, 53, 59]
 console.log(arr)
 
 console.log('\nbinarySearch')
-console.log('-10: ' + binarySearch(arr, -10))
-console.log('0: ' + binarySearch(arr, 0))
 console.log('1: ' + binarySearch(arr, 1))
-console.log('5: ' + binarySearch(arr, 5))
-console.log('5.5: ' + binarySearch(arr, 5.5))
-console.log('6: ' + binarySearch(arr, 6))
-console.log('7: ' + binarySearch(arr, 7))
-console.log('35: ' + binarySearch(arr, 35))
-console.log('36: ' + binarySearch(arr, 36))
-console.log('37: ' + binarySearch(arr, 37))
-console.log('100: ' + binarySearch(arr, 100))
+console.log('2: ' + binarySearch(arr, 2))
+console.log('3: ' + binarySearch(arr, 3))
+console.log('11: ' + binarySearch(arr, 11))
+console.log('33: ' + binarySearch(arr, 33))
 
 console.log('\nbinarySearchRecursive')
-console.log('5: ' + binarySearchRecursive(arr, 5))
-console.log('6: ' + binarySearchRecursive(arr, 6))
-console.log('7: ' + binarySearchRecursive(arr, 7))
+console.log('1: ' + binarySearchRecursive(arr, 1))
+console.log('2: ' + binarySearchRecursive(arr, 2))
+console.log('3: ' + binarySearchRecursive(arr, 3))
+console.log('11: ' + binarySearchFirst(arr, 11))
+console.log('33: ' + binarySearchFirst(arr, 33))
 
 console.log('\nbinarySearchClosure')
-console.log('5: ' + binarySearchClosure(arr, 5))
-console.log('6: ' + binarySearchClosure(arr, 6))
-console.log('7: ' + binarySearchClosure(arr, 7))
+console.log('1: ' + binarySearchClosure(arr, 1))
+console.log('2: ' + binarySearchClosure(arr, 2))
+console.log('3: ' + binarySearchClosure(arr, 3))
+console.log('11: ' + binarySearchFirst(arr, 11))
+console.log('33: ' + binarySearchFirst(arr, 33))
+
+console.log('\nbinarySearchFirst')
+console.log('1: ' + binarySearchFirst(arr, 1))
+console.log('2: ' + binarySearchFirst(arr, 2))
+console.log('3: ' + binarySearchFirst(arr, 3))
+console.log('11: ' + binarySearchFirst(arr, 11))
+console.log('33: ' + binarySearchFirst(arr, 33))
+
+console.log('\nbinarySearchLast')
+console.log('1: ' + binarySearchLast(arr, 1))
+console.log('2: ' + binarySearchLast(arr, 2))
+console.log('3: ' + binarySearchLast(arr, 3))
+console.log('11: ' + binarySearchLast(arr, 11))
+console.log('33: ' + binarySearchLast(arr, 33))
+
+console.log('\nbinarySearchInsert')
+console.log('1: ' + binarySearchInsert(arr, 1))
+console.log('2: ' + binarySearchInsert(arr, 2))
+console.log('3: ' + binarySearchInsert(arr, 3))
+console.log('11: ' + binarySearchInsert(arr, 11))
+console.log('33: ' + binarySearchInsert(arr, 33))
